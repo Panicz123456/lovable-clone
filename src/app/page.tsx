4919ead355986669879a5cd5e5d06ac90f0eecc8
patryk <paniczgg@gmail.com>
2025-08-06 02:57:48 +0200

    Ai Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
@@ -13,11 +15,14 @@ export default function Home() {
     }
   }));
 
+  const [value, setValue] = useState("")
+
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "John " })}
+        onClick={() => invoke.mutate({ value: value })}
       >
         Invoke a background jobs
       </Button>
